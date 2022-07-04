55c22d2d864843c99a338049e525de53d21ca982
$ whoami Jim <104743175+eljimmo@users.noreply.github.com>
2022-07-04 13:11:02 -0500

    Update main.go
    Added in a few notes for understanding the Go syntax and language structure.

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
+// Golang- the language for speed and concurrency!
+//all computers do is remember data.... and all you need to 
+//do learn is how they organize and call that data back.
+
+// Imperative programming – focuses on how to execute,
+// & defines control flows as statements that change a program and its state.
+// works with more efficiency, like a lower level language
+
+
+
 import "fmt"
 
+//standard module "fmt" to print messages to console/terminal
+
+
+//this function will automatically run anything inside the 
+//curly brackets 
 func main() {
 	fmt.Print("Hello World!")
 }
